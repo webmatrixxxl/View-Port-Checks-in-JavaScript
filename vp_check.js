75183c1b7866ab255867836d923bea5fdf713e8e
webmatrixxxl <webmatrixxxl@gmail.com>
2017-08-05 10:39:35 +0300

    Create vp_check.js

---
+++ vp_check.js
@@ -0,0 +1,23 @@
+  // Check if targetEl is in it's parent viewport
+
+  function isElInViewport(targetEl, containerEl, offset = 0) {
+    let scrollBott = containerEl.scrollTop + containerEl.clientHeight;
+    let elOffsetBott = targetEl.offsetTop + targetEl.clientHeight;
+
+    return (
+      (containerEl.scrollTop <= targetEl.offsetTop && scrollBott >= elOffsetBott) ||
+      (containerEl.scrollTop + offset >= targetEl.offsetTop && containerEl.scrollTop - offset <= elOffsetBott) ||
+      (scrollBott + offset >= targetEl.offsetTop && scrollBott - offset <= elOffsetBott)
+    );
+  }
+
+
+   // Check if targetEl is in it's parent center
+
+  function isElInViewportCenter(targetEl, containerEl) {
+    let scrollCenterPos = containerEl.scrollTop + (containerEl.clientHeight / 2),
+      elOffsetTop = targetEl.offsetTop,
+      elOffsetBott = elOffsetTop + targetEl.clientHeight;
+
+    return scrollCenterPos > elOffsetTop && scrollCenterPos < elOffsetBott;
+  }
